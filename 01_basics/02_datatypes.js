82c2fdefeb3f8560b9e8c928e1cbae66b45c2fd7
HitarthVyas <149051489+HitarthVyas@users.noreply.github.com>
2023-10-26 10:25:45 +0000

    operators

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -18,15 +18,15 @@ object
 // see specifications on tc36.es
 
 let names = "hvdas";
-console.log(typeof null) // returns object
-console.log(typeof(undefined)); // returns undefined
+// console.log(typeof null) // returns object
+// console.log(typeof(undefined)); // returns undefined
 
 let score = "32sde";
 
 let valInNum = Number(score);
 // String()
 
-console.log(typeof valInNum);
+// console.log(typeof valInNum);
 
 //  NaN => datatype => number
 /*
@@ -36,6 +36,28 @@ in boolean:
 "" => false
 "hvdas" => true
 */
+
+
+
+// ****************OPERATIONS*****************
+
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 1 + "2");
+// console.log(1 + "2");
+
+// console.log(+true);
+// console.log(+true);
+// console.log(+"");
+
+// let n1, n2, n3;
+// n1 = n2 = n3 = 9;
+// console.log(n1, n2, n3);
+
+
+
+
+
 
 
 // tc39.es is official js documentation site
---
+++ 01_basics/03_operators.js
@@ -0,0 +1,11 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
+//  browser is changing the value
+
+
+// == checks value only
+// === is known as strict check as it checks value and datatype also
